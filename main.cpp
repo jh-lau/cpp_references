53451154b855588f3d9833aff40d26bc830778a5
lorgerd <lorgerd@163.com>
2021-03-23 17:11:38 +0800

    feat: add source files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by liujianhan on 21/3/23 16:37.
+//
+#include "src/chp_1//func.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+//    printName();
+    calcAverage2();
+}
---
+++ src/chp_1/func.h
@@ -0,0 +1,11 @@
+#pragma once
+
+int printName();
+
+int greetWithName();
+
+int greetWithName2();
+
+int calcAverage();
+
+int calcAverage2();

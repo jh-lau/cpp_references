72a603b6a357620cba02c3e9891a9c3d90d58269
lorgerd <lorgerd@163.com>
2021-03-23 11:25:46 +0800

    feat: add source files.

---
+++ src/chapter_1/chapter_1/demo_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main1()
+{
+	cout << "Please Enter your first name:";
+	string user_name;
+	cin >> user_name;
+	cout << '\n'
+		<< "Hello, "
+		<< user_name
+		<< " ... and goodbye!\n";
+
+	return 0;
+}
---
+++ src/chapter_1/chapter_1/demo_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int num_tries = 0, num_right = 0;
+	double user_socre = 0.;
+	char usr_more;
+	bool go_for_it = true;
+	string user_name;
+	user_name;
+	const int max_tries = 3;
+	const double pi = 3.14159;
+	cout << "Try another sequence? Y/N ";
+	cin >> usr_more;
+
+	return 0;
+}
